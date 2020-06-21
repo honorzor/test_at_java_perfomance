4eca5f876f67afb84a741ceb5a443ec39138025a
ioann <ioanntrapeznikov@gmail.com>
2020-06-21 13:15:06 +0300

    change

--- src/main/java/ru/honorzor/day1/Application.java
+++ src/main/java/ru/honorzor/day1/Application.java
@@ -1,4 +1,4 @@
-package ru.honorzor;
+package ru.honorzor.day1;
 
 public class Application {
     public static void main(String[] args) {
---
+++ src/main/java/ru/honorzor/finaltask1/MailMessage.java
@@ -0,0 +1,33 @@
+package ru.honorzor.finaltask1;
+
+public class MailMessage {
+    private String from;
+    private String to;
+    private String message;
+
+    public MailMessage(String from, String to, String message) {
+        this.from = from;
+        this.to = to;
+        this.message = message;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
